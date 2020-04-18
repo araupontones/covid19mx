e2b8b1faaa509b72cf0c585e07a2532de5171702
araupontones <43657130+araupontones@users.noreply.github.com>
2020-04-18 18:05:36 +0200

    read data directly from github

--- app/server.R
+++ app/server.R
@@ -16,7 +16,7 @@ server <- function(input, output, session) {
 
   ##Parameters -----------------------------------------------------------------------------------------------------------
   
-  
+  downloadGit = "https://github.com/araupontones/covid19mx/blob/master/downloads/download_datos_abiertos.R?raw=TRUE"
   
   Sys.setlocale("LC_TIME", "Spanish")
   #fuente="obtenidos a través del paquete <i>covidMex</i> (Reyes,2020) y el repositario <i>covid19_mex</i> (Guzmán, 2020)"
@@ -34,162 +34,12 @@ server <- function(input, output, session) {
   
   
   
-  ## Download data from  -------------------------------------------------------------------------------------------------
-  
-  ##Andres Arau : https://github.com/araupontones
-  ## Fecha: 14 de abril (Dia de la Republica!)
-  ##Objetivo 1: Descargar datos publicados gob.mx/datos abiertos/datos_abiertos_covid19.zip
-  ##Objetivos 2": Leer tabla de casos en formato csv
-  ##Objetivo 3: Leer diccionario de variables
-  ##Objetivo final: Tener una base de datos limpia con varibles en formato factor que permitan el analisis de la tabla publicada
+## Run script to download data from github -------------------------------------------------------------------------------------------------
   
   
-  
-  ###1.Definir parametros para descargar, unzip y leer archivo de casos ---------------------------------------------------------
-  
-  #link = "https://www.gob.mx/salud/documentos/datos-abiertos-152127"
-  href = "http://187.191.75.115/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip" 
-  csv = "COVID19_Mexico_"
-  zip_ = "casos.zip"
-  tempdir = tempdir()
-  zip_dir=file.path(tempdir, zip_)
-  unzip_dir = file.path(tempdir, "Casos_confirmados")
-  
-  
+  source(downloadGit)
 
-  
-  ###2.Descargar el arhivo .zip de casos ------------------------------------------------------------------------------
-  
-  RawData = GET(href,
-            add_headers(Connection = 'keep-alive',
-                        `Upgrade-Insecure-Requests`="1",
-                        `User-Agent`= 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'))
-  
-  filecon <- file(zip_dir, "wb")  ##oabrir conexion con directorio temporal
-  writeBin(RawData$content, filecon)  ###descargar directorio zip en directorio temporal!!
-  close(filecon) ##cerrar conexion
-  
-  
-  ##unzip el directorio en directorio temporal
-  unzip(zip_dir, overwrite = T,
-        exdir = unzip_dir)
-  
-  #leer el CSV de casos confirmados
-  casos_df = read_csv(file.path(unzip_dir,
-                                list.files(unzip_dir, pattern = ".csv")[1])
-  )
-  
-  
-  
-  
-  
-  ###3.Descargar el arhivo .zip de diccionario ---------------------------------------------------------------------------------------
- 
-  lista_diccionarios = read_rds("lista_diccionarios.rds")
-  
-  
-  ###4. Crear look up tables para re-labear los factores de Municipio y Estado de residencia ---------------------------------------
-  
-  ##look up de entidades y municipios
-  entidades= lista_diccionarios$ENTIDADES
-  municipios = lista_diccionarios$MUNICIPIOS %>%
-    mutate(MUN_CLAVE = paste0(CLAVE_ENTIDAD, CLAVE_MUNICIPIO)) ##crear identificacion pod Municipio
-  
-  
-  
-  ##CREAR ID DE MUNICIPIOS
-  casos_df = casos_df %>%
-    mutate(MUNICIPIO_RES = paste0(ENTIDAD_RES,MUNICIPIO_RES)) 
-  #%>%
-  # filter(MUNICIPIO_RES!="19066") #Eliminar inconsistencia de NL (envie tweet para confirmar)
-  
-  
-  ##levels y labels de entidad 
-  levels_entidad = entidades$CLAVE_ENTIDAD
-  label_entidad = entidades$ENTIDAD_FEDERATIVA
-  
-  
-  ##levels and labels de municipio
-  label_municipios = municipios$MUNICIPIO
-  levels_municipos = paste0(municipios$CLAVE_ENTIDAD,municipios$CLAVE_MUNICIPIO)
-  
-  ##levels and labels tipo paciente
-  label_paciente = lista_diccionarios$TIPO_PACIENTE$DESCRIPCION
-  level_paciente = lista_diccionarios$TIPO_PACIENTE$CLAVE
-  
-  ##levels and labels tipo sector
-  label_sector = lista_diccionarios$SECTOR$DESCRIPCION
-  level_sector = lista_diccionarios$SECTOR$CLAVE
-  
-  ##levels and labels origen
-  label_origen = lista_diccionarios$ORIGEN$DESCRIPCION
-  level_origen = lista_diccionarios$ORIGEN$CLAVE
-  
-  ##levels and labels SINO
-  label_sino = lista_diccionarios$SI_NO$DESCRIPCION
-  level_sino = lista_diccionarios$SI_NO$CLAVE
-  
-  ##funcion para variables sino
-  sino = function(x){factor(x,
-                            levels = level_sino,
-                            labels = label_sino)}
-  
-  
-  ##variables sino
-  sino_vars = c("INTUBADO", "NEUMONIA", "EMBARAZO", 
-                "HABLA_LENGUA_INDI", "DIABETES", 
-                "EPOC", "ASMA", "INMUSUPR","HIPERTENSION",
-                "OTRA_CON","CARDIOVASCULAR", "OBESIDAD",
-                "RENAL_CRONICA","TABAQUISMO", "OTRO_CASO",
-                "MIGRANTE", "UCI")
-  
-  
-  
-  ###5. convertir variables en factores utilizando diccionario ------------------------------------------------------------------------
-  
-  tabla_casos_covid = casos_df %>%
-    mutate(ENTIDAD_NAC = factor(ENTIDAD_NAC,
-                                levels = levels_entidad,
-                                labels = label_entidad),
-           ENTIDAD_UM = factor(ENTIDAD_UM,
-                               levels = levels_entidad,
-                               labels = label_entidad),
-           ENTIDAD_RES = factor(ENTIDAD_RES,
-                                levels = levels_entidad,
-                                labels = label_entidad),
-           MUNICIPIO_RES_ID = MUNICIPIO_RES,
-           MUNICIPIO_RES = factor(MUNICIPIO_RES,
-                                  levels = levels_municipos,
-                                  labels = label_municipios),
-           SEXO = factor(SEXO,
-                         levels = c("1","2", "99"),
-                         labels = c("MUJER", "HOMBRE", "NO ESPECIFICADO")),
-           RESULTADO = factor(RESULTADO,
-                              levels = c("1","2","3"),
-                              labels = c("Positivo SARS-CoV-2","No positivo SARS-CoV-2","Resultado pendiente")),
-           NACIONALIDAD = factor(NACIONALIDAD,
-                                 levels = c("1", "2", "99"),
-                                 labels = c("MEXICANA", "EXTRANJERA", "NO ESPECIFICADO")),
-           TIPO_PACIENTE = factor(TIPO_PACIENTE,
-                                  levels = level_paciente,
-                                  labels = label_paciente),
-           SECTOR = factor(SECTOR,
-                           levels = level_sector,
-                           labels = label_sector),
-           ORIGEN = factor(ORIGEN,
-                           levels = level_origen,
-                           labels = label_origen)
-    ) %>%
-    ##variables si_no
-    mutate_at(sino_vars, sino)
-  
-  
-  
-  
-  
-  ### 6. Remover elementos utilizados durante la limpieza y descarga de la tabla--------------------------------------------------------
-  rm(casos_df, entidades, lista_diccionarios, municipios, RawData)
-  
+##limpiar datos ------------------------------------------------------------------------------------------------------ 
   ##manterner solamente casos confirmados Y hacer tabla consistente con version previa
   table = subset(tabla_casos_covid, RESULTADO =="Positivo SARS-CoV-2") %>%
     rename(Edad = EDAD,
